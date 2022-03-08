e4b540979d1d0713692adf8c079506cade4ccab2
kedaroo <krb1202@gmail.com>
2022-03-08 17:31:58 +0530

    style: header & hero-img

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    alert("The error occured: " + error);
+}
+
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    outputDiv.innerText = "hlbadnsc basda" + txtInput.value;
+    var userInput = txtInput.value;
+    var url = getTranslationURL(userInput);
+    fetch(url)
+    .then(res => res.json())
+    .then(json => outputDiv.innerText = json.contents.translated)
+    .catch(errorHandler)
 }); 
